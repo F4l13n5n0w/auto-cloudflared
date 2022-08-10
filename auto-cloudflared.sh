30842825c2b9ced691d5308bd4e56baa7ff988b2
Max <maxcaminer1@gmail.com>
2022-08-10 21:26:27 +1000

    fixed english

--- auto-cloudflared.sh
+++ auto-cloudflared.sh
@@ -54,8 +54,8 @@ cloudflared tunnel route dns $tunnel_name $fqdn
 
 echo -n "[?] What port would you like to run the web server on? "
 read port
-echo "[!] In a new terminal window/tab, run the following command: python3 -m http.server $port" &
-echo "[!] Once the web server is start, in a web browser, navigate to the following address: https://$fqdn/"
+echo "[!] In a new terminal window/tab, run the following command: python3 -m http.server $port"
+echo "[!] Once the web server starts then open a web browser and navigate to the following address: https://$fqdn/"
 cloudflared tunnel run --url localhost:$port $tunnel_name &
 # Uncomment the line below if you want to automatically start web server
 # python3 -m http.server $port &
